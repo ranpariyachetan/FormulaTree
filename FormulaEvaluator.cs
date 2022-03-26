b0afc7c430dc0749acbdd88cc3aa0705505ca798
Chetanbhai Ranpariya <chetanbhai.ranpariya@autodesk.com>
2022-03-26 21:40:22 +0800

    Evaluating formula with parantheses.
    Constraint
    - The formula does not have any error
    - Every pair of parentheses has exactly two operands and one operator.

--- FormulaEvaluator.cs
+++ FormulaEvaluator.cs
@@ -1,59 +1,79 @@
-namespace FormaulaTree;
-
-public class FormaulaEvaluator
+namespace FormaulaTree
 {
-    private static List<char> operators = new List<char> {'+', '-', '*', '/'};
-    public double EvaluateFormula(string input)
+    public class FormaulaEvaluator
     {
-		double op1 = 0d;
-		double op2 = 0d;
-		var op1Found = false;
-		char op = '\0' ;
-		double answer = 0;
-		for(var i = 0;i<input.Length;i++)
-		{
-			var c = input[i];
-			if(c != ' ')
-			{
-				if(operators.Contains(c))
-				{
-					op = c;
-				}
-				else if(Char.IsNumber(c))
-				{
-					if(!op1Found)
-					{
-						op1 = Convert.ToDouble(c.ToString());
-						op1Found = true;
-						continue;
-					}
-					else
-					{
-						op2 = Convert.ToDouble(c.ToString());
-						answer = PerformCalculation(op1, op2, op);
-						op1 = answer;
-					}
-				}
-			}
-		}
+        private static List<char> operators = new List<char> {'+', '-', '*', '/'};
 
-        return answer;
+        public double EvaluateFormula(string input)
+        {
+            var stk = new Stack<string>();
+
+            input = $"({input})";
+            double answer = 0;
+            for(var i = 0;i<input.Length;i++)
+            {
+                var c = input[i];
+                if(c != ' ')
+                {
+                    if(operators.Contains(c))
+                    {
+                        stk.Push(c.ToString());
+                    }
+                    else if(c == '(')
+                    {
+                        stk.Push(c.ToString());
+                    }
+                    else if(Char.IsNumber(c))
+                    {
+                        var d = Convert.ToDouble(c.ToString());
+                        var j = i + 1;
+                        while(Char.IsNumber(input[j]))
+                        {
+                            d = (d * 10) + Convert.ToDouble(input[j].ToString());
+                            j++;
+                        }
+                        i = j-1;
+                        stk.Push(d.ToString());
+                    }
+                    else if(c == ')')
+                    {
+                        while(stk.Count > 0)
+                        {
+                            var op2 = Convert.ToDouble(stk.Pop());
+                            var op = stk.Pop();
+                            var op1 = Convert.ToDouble( stk.Pop());
+
+                            answer = PerformCalculation(op2, op1, op);
+
+                            if(stk.Peek() == "(")
+                            {
+                                stk.Pop();
+                                stk.Push(answer.ToString());    
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return Convert.ToDouble(stk.Pop());
+        }
+
+        private static double PerformCalculation(double op1, double op2, string oper)
+        {
+            switch (oper)
+            {
+                case "+":
+                    return op1 + op2;
+                case "-":
+                    return op1 - op2;
+                case "*":
+                    return op1 * op2;
+                case "/":
+                    return op1 / op2;
+                default:
+                    return 0;
+            }
+        }
     }
-
-    private static double PerformCalculation(double op1, double op2, char oper)
-	{
-		switch (oper)
-		{
-			case '+':
-				return op1 + op2;
-			case '-':
-				return op1 - op2;
-			case '*':
-				return op1 * op2;
-			case '/':
-				return op1 / op2;
-			default:
-				return 0;
-		}
-	}
 }
