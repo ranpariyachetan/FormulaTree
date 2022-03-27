c93f8df072efe37599453bc2aaaf482043a515bb
Chetanbhai Ranpariya <chetanbhai.ranpariya@autodesk.com>
2022-03-27 10:13:56 +0800

    Added code to display json of tree.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿public class Program
+﻿using System.Text.Json;
+public class Program
 {
     public static void Main(string[] args)
     {
@@ -7,6 +8,15 @@
         // var input = "(15 + (7 - (1 + 1) ) ) * 3 ) - (2 + (1 + 1)";
         var result = evaluator.CreateTree(input);
 
+        var options = new JsonSerializerOptions { WriteIndented = true, Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping, IgnoreNullValues = true };
+
+        string jsonString = JsonSerializer.Serialize(result, options);
+
+
         Console.WriteLine(result.Calculate());
+
+        Console.WriteLine(jsonString);
+
+        
     }
 }
